905e2d53a56e20079d7809604ea9a51e6e354a96
gazi275 <gaziazad270@gmail.com>
2023-11-25 09:45:47 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './Root.jsx';
+import ErrorPage from './error/ErrorPage.jsx';
+import Home from './Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element:<Home></Home>
+      },
+    ],
+  },
+]);
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
