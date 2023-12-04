55d13b380906b9a454bee703a38e1ee10e691349
gazi275 <gaziazad270@gmail.com>
2023-12-04 13:57:02 +0600

    announcement showing

--- src/Home/Announments.jsx
+++ src/Home/Announments.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 
+
 const Announments = () => {
   const [postData, setPostData] = useState([]);
   useEffect(() => {
@@ -26,9 +27,33 @@ const Announments = () => {
 console.log(postData);
 
     return (
-        <div>
+        <div className="mt-20">
+<div>
+  {postData?.length==0 ? <div className="hidden"></div>
+  : <div>
+    <div>     
+<h1 className="mb-4 text-4xl font-extrabold leading-none text-center tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">The Announcement</h1>
+<p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">we focus on markets where technology, innovation, and capital can unlock long-term value and drive social growth.</p>
 
-{ann}
+
+    </div>
+
+  { postData?.map(data=>
+   <div key={data.id} className="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto my-5">
+   <div className="bg-blue-600 bg-[url('https://preline.co/assets/svg/examples/abstract-1.svg')] bg-no-repeat bg-cover bg-center p-4 rounded-lg text-center">
+     <p className="me-2 inline-block text-white">
+      {data.announce}
+     </p>
+     
+   </div>
+ </div> )}
+ 
+
+
+
+    </div>}
+</div>
+
 
 
         </div>
